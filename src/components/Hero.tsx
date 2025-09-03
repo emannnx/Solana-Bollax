91ffef14fd9235eec36d520b003ceacb34d918ee
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2025-09-03 22:33:30 +0100

    4 commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
                 <div className="w-10 h-10 rounded-lg gradient-primary flex items-center justify-center">
                   <Shield className="h-5 w-5 text-blue-foreground" />
                 </div>
-                <h1 className="text-2xl font-bold text-gradient">
+                <h1 className="text-2xl font-bold text-blue-600">
                   Bollax-Solana
                 </h1>
               </div>
@@ -49,7 +49,7 @@ const Hero = () => {
                 </div>
                 
                 <h2 className="text-6xl md:text-8xl font-bold leading-tight">
-                  <span className="text-gradient block mb-4">
+                  <span className="text-blue-600 block mb-4">
                     Analyze Solana
                   </span>
                   <span className="text-foreground/90">Meme Coins</span>
